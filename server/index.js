bc62d9eacb4a906c1fde730b54c297f076a395a1
Carlos Villuendas <carlos.villuendas@softonic.com>
2014-11-06 18:04:17 +0100

    minor fix

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 var express = require( 'express' ),
-		app = express();
+		app = express(),
+		fs = require('fs'),
+		path = require( 'path' );
 
-app.get( '/nombres', function( req, res, next ){
-	res.json( [ 'Guille', 'Carlos' ] );
+app.get( '/', function( req, res, next ){
+	res.set('Content-Type', 'text/html');
+	fs.createReadStream( path.join( __dirname, '..', 'client', 'index.html' ) ).pipe( res );
 } );
 
 module.exports = app;
